6ac10d7e2231dc6723d703e8465fed8cf27215a3
KravKo <krishavkoirala99@gmail.com>
2025-01-12 16:27:02 +0545

    krishav changes

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Home from '../layout/Home';
 import OurMission from '../layout/OurMission';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 export default function LandingPage() {
   return (
