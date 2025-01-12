291c7158ec38bc5927beee11c2b88810912e460f
KravKo <krishavkoirala99@gmail.com>
2025-01-12 15:31:08 +0545

    added a link and used navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import LogoWhite from '../assets/logoForBlack.png';
+import { Link, useNavigate  } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const loginClick = () => {
+    navigate('/login'); 
+  }
   return (
     <>
       <div className='main bg-[#28282B] flex items-center space-x-20 py-2 justify-between w-[100%] sticky top-0 z-10'>
@@ -19,7 +24,7 @@ export default function Navbar() {
               </ul>
           {/* Login */}
           <div className='flex flex-col justify-center  w-[10%] text-white text-xl font-semibold space-y-2'>
-            <button className='border-4 border-[#FCDFDF] rounded-3xl'>Log In</button>
+            <button onClick={loginClick} className='border-4 border-[#FCDFDF] rounded-3xl'>Log In</button>
           </div>
         </div>
 
