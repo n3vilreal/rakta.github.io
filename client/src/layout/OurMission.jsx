6e62f46608902e4be9e049f23974b6ee4a6ba715
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-12 16:09:35 +0545

    null

--- client/src/layout/OurMission.jsx
+++ client/src/layout/OurMission.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image1 from '../assets/manDonatingBlood.jpg';
+import Image2 from '../assets/bloodBag.jpg';
 
 export default function OurMission() {
   return (
@@ -12,7 +13,7 @@ export default function OurMission() {
         </div>
 
         {/* Missions */}
-        <div className='missions flex flex-col mt-10 space-y-8'>
+        <div className='missions flex flex-col space-y-8'>
             
             {/* Mission 1 */}
             <div className="mission-top w-full flex">
@@ -29,7 +30,12 @@ export default function OurMission() {
             </div>
 
             {/* Mission 2 */}
-            <div className="mission-top w-full flex">
+            <div className="mission-bottom w-full flex">
+
+                <div className='flex w-[50%] justify-center'>
+                    <img src={Image2} className='h-[400px]' />
+                </div>
+
                 <span className=" info1 w-[500px] h-[400px] text-2xl py-12 px-10 bg-[#d84040] text-white leading-loose" 
                     style={ {fontFamily: "'Saira Condensed', sans-serif" }}>
                     Rakta is a project based for Health related sector.
@@ -37,12 +43,7 @@ export default function OurMission() {
                     Every individual could not buy blood bag due to high cost for them.
                     Here our web application plays a vital role in connecting donors and acceptors. 
                 </span>
-                <div className='flex w-[50%] justify-center'>
-                    <img src={Image1} className='h-[400px]' />
-                </div>
             </div>
-
-            
 
         </div>
     </div>
