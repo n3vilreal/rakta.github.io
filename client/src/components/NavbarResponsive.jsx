a325cf5048a3759960723d59ae35510d50c12d4b
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-12 22:42:55 +0545

    responsive nav

--- client/src/components/NavbarResponsive.jsx
+++ client/src/components/NavbarResponsive.jsx
@@ -5,26 +5,29 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import Navigation from "./Navigation";
 
 export default function Navbar() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
   const loginClick = () => {
     navigate("/login");
   };
   return (
     <>
-      <div className="main bg-[#28282B] flex items-center space-x-20 py-2 justify-between w-[100%] sticky top-0 z-10 md:hidden">
+      <div className="main bg-[#28282B] flex items-center py-2 w-[100%] sticky top-0 z-10
+                      md:hidden">
         {/* Logo */}
-        <img src={LogoWhite} alt="" className="h-14 ml-6" />
+        <Navigation />
+        <img src={LogoWhite} alt="" className="h-14 ml-6 " />
         <div className="flex justify-end space-x-8 pr-5 w-[75%]">
           {/* Login */}
           <div className="flex items-center space-x-7 text-white text-md font-semibold">
             <button
               onClick={loginClick}
-              className="bg-red-600 rounded-xl px-3 cursor-pointer hover:text-red-600 hover:bg-white ease-in-out transition-all duration-300"
-            >
+              className="bg-red-600 rounded-xl px-3 cursor-pointer
+               hover:text-red-600 hover:bg-white ease-in-out transition-all duration-300">
               Log In
             </button>
             
-            <button>
+            <button className="flex items-center space-x-2"
+                onClick={toggleNav}>
                 <GiHamburgerMenu />
             </button>
           </div>
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -11,8 +11,9 @@ export default function Navigation() {
   return (
     
     <>
-      <ul className="flex text-white text-xl font-semibold w-[80%] space-x-6 
-                    max-md:flex-col max-md:bg-red-300 max-md:font-medium max-md:space-x-0">
+      <ul className="flex text-white text-xl font-semibold w-[80%] space-x-6
+                    max-md:flex-col max-md:bg-inherit max-md:pl-4 max-md:pt-4 max-md:font-medium max-md:space-x-0
+                    max-md:absolute max-md:top-[72px] max-md:right-0 max-md:w-[100vw] max-md:h-[100vh] max-md:space-y-4">
             <span
               className="cursor-pointer hover:text-red-500 transition-all duration-600 ease-in-out"
               onClick={() => handleRedirectAndScroll("Home")}
@@ -25,7 +26,6 @@ export default function Navigation() {
             >
               Our Mission
             </span>
-
             <span
               className="cursor-pointer hover:text-red-500 transition-all duration-600 ease-in-out"
               onClick={() => handleRedirectAndScroll("map")}
