d46b0d0b80d93aa6a2b17424d301826663728953
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-12 23:20:23 +0545

    hr

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import NavbarResponsive from '../components/NavbarResponsive';
+
+export default function Profile() {
+  return (
+    <>
+        <Navbar />
+        <NavbarResponsive />
+    </>
+  )
+}
