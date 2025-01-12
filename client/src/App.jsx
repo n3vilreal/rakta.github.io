f2e1d2dfeb4424f89b78f836812403115da473b2
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-13 00:27:50 +0545

    Profile Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Profile from "./pages/Profile";
 import { AuthProvider } from "./contexts/authContext/Index";
 import Login from "./pages/Login";
 
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import NavbarResponsive from '../components/NavbarResponsive';
+import MyProfile from '../components/MyProfile';
 
 export default function Profile() {
   return (
     <>
         <Navbar />
-        <NavbarResponsive />
+        <NavbarResponsive /> 
+        <MyProfile />
     </>
   )
 }
