dfdcc4696f13399a16fc20d81400ff525aa85187
KravKo <krishavkoirala99@gmail.com>
2025-01-13 11:05:01 +0545

    fixed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,32 +25,35 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="main bg-[#28282B] flex items-center space-x-20 py-2 justify-between w-[100%] sticky top-0 z-10 max-md:hidden">
+      <div className="main bg-[#28282B] flex items-center py-2 justify-between w-[100%] sticky top-0 z-10 max-md:hidden">
         {/* Logo */}
-        <img src={LogoWhite} alt="" className="h-24 ml-6" />
-          {/* Navigation */}
+        <div className="flex items-center">
+          <img src={LogoWhite} alt="" className="h-24 ml-6" />
+        </div>
+        
+        {/* Navigation */}
+        <div className="flex-grow flex justify-center">
+          <Navigation />
+        </div>
 
-          <div className="justify-center items-center flex w-full">
-          <Navigation/>
-          </div>
-          {/* Login */}
-          <div className="flex flex-col justify-center  w-[10%] text-white text-xl font-semibold space-y-2">
+        {/* Login/Logout */}
+        <div className="flex items-center justify-end text-white text-xl font-semibold space-x-4 mr-6">
           {userLoggedIn ? (
-              <button
-                onClick={logoutClick}
-                className="bg-red-600 py-1 rounded-xl cursor-pointer hover:text-red-600 hover:p-2 hover:bg-white ease-in-out transition-all duration-300"
-              >
-                Log Out
-              </button>
-            ) : (
-              <button
-                onClick={loginClick}
-                className="bg-red-600 py-1 mr-4 rounded-xl cursor-pointer hover:text-red-600 hover:p-2 hover:bg-white ease-in-out transition-all duration-300"
-              >
-                Log In
-              </button>
-            )}
-          </div>
+            <button
+              onClick={logoutClick}
+              className="bg-red-600 py-1 px-4 rounded-xl cursor-pointer hover:text-red-600 hover:bg-white ease-in-out transition-all duration-300"
+            >
+              Log Out
+            </button>
+          ) : (
+            <button
+              onClick={loginClick}
+              className="bg-red-600 py-1 px-4 rounded-xl cursor-pointer hover:text-red-600 hover:bg-white ease-in-out transition-all duration-300"
+            >
+              Log In
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
